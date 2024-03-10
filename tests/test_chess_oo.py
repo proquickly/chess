8b86cedac948b6623b8a95ee0cadbd55a431eaa2
Andy Miles <akmiles@icloud.com>
2024-03-10 10:00:52 -0700

    reorged project so tests now running

---
+++ tests/test_chess_oo.py
@@ -0,0 +1,25 @@
+from chess_oo import chess_oo
+
+
+def test_create_pawn():
+    pawn = chess_oo.Pawn("white")
+    assert isinstance(pawn, chess_oo.Pawn)
+
+
+def test_pawn_valid_move():
+    start_square = ("a", "2")
+    end_square = ("a", "3")
+    p = chess_oo.Pawn("white")
+    assert p.is_valid_move(start_square, end_square)
+
+
+def test_is_square_occupied():
+    b = chess_oo.Game()
+    square = ("a", "1")
+    assert not b.is_square_empty(square)
+
+
+def test_is_square_empty():
+    b = chess_oo.Game()
+    square = ("a", "3")
+    assert b.is_square_empty(square)
