516ba78448e8e9bcc417d34de514fcc9f6954d12
Pawel <paul.szyburski@gmail.com>
2025-01-26 17:27:47 +0100

    Filled out the app function.

--- src/chess/chess_client.py
+++ src/chess/chess_client.py
@@ -1,18 +1,29 @@
 import chess_server
-# import keywords
+#import keywords
 import vectordb
 
 def show_menu():
     print("Chess")
     print("1. Play")
     print("2. Ask chess AI")
-    print("3. Maintain data")
+    print("3. Maintain data")# Dont know exactly what that means.
     print("9. Exit")
     choice = input("Enter your choice: ")
     return choice
 
 def app():
     choice = show_menu()
+
+    if choice == "1":
+        chess_server.play_game()
+    elif choice == "2":
+        vectordb.main()
+    elif choice == "3":
+        pass
+    elif choice == "9":
+        exit()
+
+
 
 
 if __name__ == "__main__":
